19171066bb4232c100561af7227cb87cffbadd05
Qusai Ghabrah <qusai.ghabrah@gmail.com>
2021-02-06 21:37:00 -0500

    Part 2 Basic Features Implemented

---
+++ Flixter/Cells/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Flixter
+//
+//  Created by csuser on 2/6/21.
+//
+
+import UIKit
+import AlamofireImage
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
--- Flixter/ViewControllers/MovieGridViewController.swift
+++ Flixter/ViewControllers/MovieGridViewController.swift
@@ -1,25 +1,34 @@
 //
-//  MoviesViewController.swift
+//  MovieGridViewController.swift
 //  Flixter
 //
-//  Created by Qusai Ghabrah on 1/29/21.
+//  Created by csuser on 2/6/21.
 //
 
 import UIKit
 import AlamofireImage
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class MovieGridViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    
+    
+    @IBOutlet weak var collectionView: UICollectionView!
     var movies = [[String:Any]]()
-
-    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.delegate = self
-        tableView.dataSource = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
         
-        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        let width = (view.frame.size.width - 2 * layout.minimumInteritemSpacing) / 3
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=f7be9eeda0167a0c945faec7987ab512")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -31,7 +40,9 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
             
             self.movies = dataDictionary["results"] as! [[String: Any]]
             
-            self.tableView.reloadData()
+            self.collectionView.reloadData()
+            
+           
 
               // TODO: Get the array of movies
               // TODO: Store the movies in a property to use elsewhere
@@ -41,33 +52,28 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         }
         task.resume()
 
-        // Do any additional setup after loading the view.
     }
     
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return movies.count
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
         
-        let movie = movies[indexPath.row]
-        let title = movie["title"] as! String
-        let synopsis = movie["overview"] as! String
-        
-        cell.titleLabel.text = title
-        cell.synopsisTitle.text = synopsis
+        let movie = movies[indexPath.item]
         
         let baseUrl = "https://image.tmdb.org/t/p/w185"
         let posterPath = movie["poster_path"] as! String
         let posterUrl = URL(string: baseUrl + posterPath)
         
         cell.posterView.af_setImage(withURL: posterUrl!)
+
+        
         
         return cell
-        
     }
+    
 
     /*
     // MARK: - Navigation
